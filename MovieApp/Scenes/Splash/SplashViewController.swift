19d31efd805bcdf8692e3d20092ce2aaea84db47
Gizem Duman <45661081+dumanngizem@users.noreply.github.com>
2022-09-22 02:10:52 +0300

    init project

---
+++ MovieApp/Scenes/Splash/SplashViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MovieApp
+//
+//  Created by Gizem Duman on 22.09.2022.
+//
+
+import UIKit
+
+class SplashViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
